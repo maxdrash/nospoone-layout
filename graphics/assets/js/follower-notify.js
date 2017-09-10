3179185111b775590ad65e4931ad2c47b5ff9126
Roméo Jr. Labonté <romeo@q-bit.ca>
2017-09-10 11:21:13 -0400

    adjusted sizes and animations

--- graphics/assets/js/follower-notify.js
+++ graphics/assets/js/follower-notify.js
@@ -1,7 +1,7 @@
 var queue = [];
 var processing = false;
 
-TweenMax.set('.container', {x: -($('.container').outerWidth(true, true) + 10 + 2)});
+TweenMax.set('.container', {x: -($('.container').outerWidth(true, true) + 10 + 8)});
 
 nodecg.listenFor('follow', user => {
 	queue.push(user);
@@ -13,7 +13,7 @@ nodecg.listenFor('follow', user => {
 
 function show() {
 	$('.container span.content').text(queue.shift());
-	const width = $('.container').outerWidth(true, true) + 10 + 2;
+	const width = $('.container').outerWidth(true, true) + 10 + 8;
 	TweenMax.set('.container', {x: -width});
 	TweenMax.to('.container', 0.5, {x: 0, delay: 1, ease: Elastic.easeOut.config(0.3, 0.4), onStart: () => {
 		nodecg.playSound('follow');
--- graphics/assets/js/top-bar.js
+++ graphics/assets/js/top-bar.js
@@ -24,8 +24,8 @@ function show() {
 		nodecg.playSound('donation');
 	}, onComplete: () => {
 		$('.container').addClass('show');
-		TweenMax.to('.container span', 0.5, {y: 0, delay: 0.25, ease: Elastic.easeOut.config(0.3, 0.4)});
-		TweenMax.to('.container span', 0.5, {y: 144, delay: 5.25, ease: Elastic.easeIn.config(0.3, 0.4), onStart: () => {
+		TweenMax.to('.container span', 0.5, {y: 0, delay: 0.5, ease: Elastic.easeOut.config(0.3, 0.4)});
+		TweenMax.to('.container span', 0.5, {y: 144, delay: 5.5, ease: Elastic.easeIn.config(0.3, 0.4), onStart: () => {
 			setTimeout(() => {
 				nodecg.playSound('leave');
 			}, 150);
