a9ae70ebfdc31f326659646d658d1cabf46f0d88
Roméo Jr. Labonté <romeo@q-bit.ca>
2017-09-10 13:46:32 -0400

    music notification (only gpmdp now)

---
+++ graphics/assets/js/music-notify.js
@@ -0,0 +1,12 @@
+TweenMax.set('.container', {x: -($('.container').outerWidth(true, true) + 10 + 8)});
+
+nodecg.listenFor('gpmdp', 'nospoone-gpmdp-fetcher', data => {
+	$('.container__title').text(data.title);
+	$('.container__artist').text(data.artist);
+	$('.container__coverart').css('background-image', `url(${data.albumArt})`);
+
+	const width = $('.container').outerWidth(true, true) + 10 + 8;
+	TweenMax.set('.container', {x: -width});
+	TweenMax.to('.container', 0.5, {x: 0, delay: 1, ease: Elastic.easeOut.config(0.3, 0.4)});
+	TweenMax.to('.container', 0.5, {x: -width, delay: 5, ease: Elastic.easeIn.config(0.3, 0.4)});
+});
