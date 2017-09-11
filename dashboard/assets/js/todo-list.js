93e98ebab666178a5eadcd6c8cddd12a89e66d1e
Roméo Jr. Labonté <romeo@q-bit.ca>
2017-09-10 22:24:04 -0400

    added todo list dashboard panel

---
+++ dashboard/assets/js/todo-list.js
@@ -0,0 +1,47 @@
+const template = $('.js-template').clone().detach();
+const itemsReplicant = nodecg.Replicant('todo-items', {defaultValue: []});
+
+$('.js-add').on('click', e => {
+	if (document.querySelector('.js-input').value !== undefined) {
+		itemsReplicant.value.push({
+			text: document.querySelector('.js-input').value,
+			checked: false
+		});
+		document.querySelector('.js-input').value = undefined;
+		render();
+	}
+});
+
+itemsReplicant.on('change', () => {
+	$('.js-rendered').remove();
+	if (itemsReplicant.value.length > 0) {
+		$('.js-no-items').hide();
+		itemsReplicant.value.forEach((item, index) => {
+			const itemTemplate = template.clone().removeClass('list__list-item--template js-list-item-template').addClass('js-rendered');
+			itemTemplate.find('.js-label').text(item.text);
+			itemTemplate.find('.js-delete').on('click', e => {
+				e.preventDefault();
+				e.stopPropagation();
+				removeItem(item);
+			});
+			itemTemplate.find('.js-checkbox').on('click', e => {
+				e.preventDefault();
+				e.stopPropagation();
+				toggleChecked(index);
+			});
+			itemTemplate.find('.js-checkbox')[0].checked = item.checked;
+			itemTemplate.find('.js-label').addClass(item.checked ? 'list__list-item__label--checked' : '');
+			$('.js-list').append(itemTemplate);
+		});
+	} else {
+		$('.js-no-items').show();
+	}
+});
+
+function toggleChecked(index) {
+	itemsReplicant.value[index].checked = !itemsReplicant.value[index].checked;
+}
+
+function removeItem(itemToRemove) {
+	itemsReplicant.value = itemsReplicant.value.filter(item => item !== itemToRemove);
+}
--- graphics/assets/js/currently-playing.js
+++ graphics/assets/js/currently-playing.js
@@ -3,12 +3,12 @@ let active = false;
 nodecg.listenFor('gpmdp', 'nospoone-gpmdp-fetcher', data => {
 	if (!active) {
 		active = true;
-		TweenMax.to('.container', 0.5, {alpha: 0, ease: Power0.easeNone, onComplete: () => {
+		TweenMax.to('.container', 0.25, {alpha: 0, ease: Power0.easeNone, onComplete: () => {
 			$('.js-title').text(data.title);
 			$('.js-artist').text(data.artist);
 			$('.js-album').text(data.album);
 			$('.js-coverart').prop('src', data.albumArt);
-			TweenMax.to('.container', 0.5, {alpha: 1, delay: 1, ease: Power0.easeNone, onComplete: () => {
+			TweenMax.to('.container', 0.25, {alpha: 1, delay: 0.5, ease: Power0.easeNone, onComplete: () => {
 				active = false;
 			}});
 		}});
